0f4fd2b92aa3d7363dc659cdb8894d4fdda33756
Aravind-B-Kumar <aravindkarullil@gmail.com>
2024-04-19 23:21:32 +0530

    View all recipe completed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import './App.css';
 import NavBar from './components/NavBar';
 import AddRecipe from './components/AddRecipe';
 import SearchRecipe from './components/SearchRecipe';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
     <div>
-      <SearchRecipe/>
+      <ViewAll/>
     </div>
   );
 }
