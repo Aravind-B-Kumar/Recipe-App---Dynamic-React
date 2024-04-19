b5417e7c91fd9ceb2926e2ca18241052fd6cb6ef
Aravind-B-Kumar <aravindkarullil@gmail.com>
2024-04-19 22:52:24 +0530

    Search and Add recipe completed

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import NavBar from './components/NavBar';
+import AddRecipe from './components/AddRecipe';
+import SearchRecipe from './components/SearchRecipe';
 
 function App() {
   return (
     <div>
-      <NavBar/>
+      <SearchRecipe/>
     </div>
   );
 }
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -5,7 +5,7 @@ const NavBar = () => {
         <div>
             <nav class="navbar navbar-expand-lg bg-body-tertiary">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="/"><img style={{height:"50px"}} src="https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Kerala_State_Road_Transport_Corporation_logo.png/200px-Kerala_State_Road_Transport_Corporation_logo.png" alt="" /></a>
+                    <a class="navbar-brand" href="/"><img style={{height:"60px"}} src="https://e7.pngegg.com/pngimages/854/415/png-clipart-recipe-cooking-chef-dish-food-cooking-food-recipe-thumbnail.png" alt="" /></a>
                     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
